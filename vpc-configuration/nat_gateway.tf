20f81fc83b322497bdf75f1e559b7151a9bfb562
Okiki-Jesu Olumide <ddonolu@yahoo.com>
2021-10-02 21:33:12 +0100

    updating network configuration

--- vpc-configuration/nat_gateway.tf
+++ vpc-configuration/nat_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_eip" "nat_eip" {
-    count = var.preferred_number_of_public_subnets == null ? length(var.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+    count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
     vpc   = true
     depends_on = [aws_internet_gateway.ig]
     tags = merge(
@@ -11,7 +11,7 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-    count = var.preferred_number_of_public_subnets == null ? length(var.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+    count = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
     allocation_id = aws_eip.nat_eip[count.index].id
     subnet_id       = element(aws_subnet.public.*.id, 0)
     depends_on      = [aws_internet_gateway.ig]
