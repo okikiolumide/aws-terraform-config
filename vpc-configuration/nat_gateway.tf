35204f9e028034f759f725bf2c9dc4df613a6112
Okiki-Jesu Olumide <ddonolu@yahoo.com>
2021-10-02 11:27:18 +0100

    adding network configuration

---
+++ vpc-configuration/nat_gateway.tf
@@ -0,0 +1,24 @@
+resource "aws_eip" "nat_eip" {
+    count = var.preferred_number_of_public_subnets == null ? length(var.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+    vpc   = true
+    depends_on = [aws_internet_gateway.ig]
+    tags = merge(
+        local.default_tags, 
+        {
+            Name = format("EIP-%s" , var.environment)
+        }
+    )
+}
+
+resource "aws_nat_gateway" "nat" {
+    count = var.preferred_number_of_public_subnets == null ? length(var.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+    allocation_id = aws_eip.nat_eip[count.index].id
+    subnet_id       = element(aws_subnet.public.*.id, 0)
+    depends_on      = [aws_internet_gateway.ig]
+    tags = merge(
+        local.default_tags , {
+            Name = format("Nat-%s", var.environment)
+        }
+    )
+}
+
