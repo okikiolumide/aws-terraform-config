a6d4c8833296e1e544e066601f43c8c554c853ee
Okiki-Jesu Olumide <ddonolu@yahoo.com>
2021-10-02 14:35:43 +0100

    adding network configuration

--- vpc-configuration/main.tf
+++ vpc-configuration/main.tf
@@ -1,17 +1,18 @@
 
 # Create VPC
 
-resource "aws_vpc" "main"{
+resource "aws_vpc" "main" {
     cidr_block                          = var.vpc_cidr
     enable_dns_support                  = var.enable_dns_support
     enable_dns_hostnames                = var.enable_dns_support
     enable_classiclink                  = var.enable_classiclink
     enable_classiclink_dns_support      = var.enable_classiclink
-    tags =
-        {
-            Name = format ("vpc-%s" , var.environment )
-        }
-    )
+    
+    # tags =
+    #     {
+    #         Name = format ("vpc-%s" , var.environment )
+    #     }
+    # )
 }
 
 # Create Public Subnet 
