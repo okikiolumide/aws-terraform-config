ebf6088728c942c9dde39e342eb0002ff2d452a5
Okiki-Jesu Olumide <ddonolu@yahoo.com>
2021-10-02 14:19:28 +0100

    adding network configuration

--- vpc-configuration/variables.tf
+++ vpc-configuration/variables.tf
@@ -1,49 +1,49 @@
-variable "region {
-    default = "eu-central-1"
+variable 'region {
+    default = 'eu-central-1'
 }
 
 # Get aws availability zones
 
-data "aws_availability_zones" "available"{
+data 'aws_availability_zones' 'available'{
     state = "available"
 }
 
-variable "vpc_cidr" {
+variable 'vpc_cidr' {
     default = "172.16.0.0/16"
 }
 
-variable "public_subnet_cidr" {
-    default = "172.16.0.0/16"
+# variable 'public_subnet_cidr' {
+#     default = "172.16.0.0/16"
+# }
+
+# variable 'private_subnet_cidr' {
+#     default = "172.16.1.0/16"
+# }
+
+variable 'enable_dns_support' {
+    default = true
 }
 
-variable "private_subnet_cidr" {
-    default = "172.16.1.0/16"
+variable 'enable_dns_hostnames'{
+    default = true
 }
 
-variable "enable_dns_support" {
-    default = "true"
+variable 'enable_classiclink'{
+    default = 'false'
 }
 
-variable "enable_dns_hostnames"{
-    default = "true"
+variable 'enable_classiclink_dns_support'{
+    default = 'false'
 }
 
-variable "enable_classiclink"{
-    default = "false"
-}
-
-variable "enable_classiclink_dns_support"{
-    default = "false"
-}
-
-variable "preferred_number_of_public_subnets"{
+variable 'preferred_number_of_public_subnets'{ 
     default = null
 }
 
-variable "preferred_number_of_private_subnets"{
+variable 'preferred_number_of_private_subnets'{
     default = null
 }
 
-variable "environment" {
+variable 'environment' {
     default = "dev"
 }
